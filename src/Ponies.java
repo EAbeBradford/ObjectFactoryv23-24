a089bfcb854d34337b78a18bd5cc2f7a205ffb6b
Mx. Bradford <emma_bradford@milton.com>
2023-10-25 09:02:14 -0400

    made default pony and made an object of pony

---
+++ src/Ponies.java
@@ -0,0 +1,13 @@
+public class Ponies {
+    // true/false -> boolean
+    //numbers -> int, double
+    //words -> String
+
+    boolean isUnicorn = false;
+    boolean isPegasus = false;
+    double size = 20.56;
+    String name = "bobby";
+    String color = "Pink";
+
+
+}
