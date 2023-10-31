bfb389006326b1defa52266a842fbd59ffb7fac5
Mx. Bradford <emma_bradford@milton.com>
2023-10-31 13:08:56 -0400

    made new pony and a describe pony method

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,23 @@
 public class Main {
+    //instaacne vars
+
+
+
     public static void main(String[] args) {
         System.out.println("hello Object Factory");
-        Ponies p = new Ponies();
+         Ponies p;
+        p = new Ponies();
         System.out.println(p.isUnicorn);
         p.isUnicorn = true;
         System.out.println(p.isUnicorn);
+        System.out.println(p.name);
+
+         p.describePony();
+
+
+        Ponies a;
+        a = new Ponies();
+        a.name = "Teddy";
+        System.out.println("pony a's name is " + a.name);
     }
 }
